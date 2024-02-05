2be5d38b07946dceac77c53d1f18db2f59834466
guanshoukui <guanshoukui@zhito.com>
2024-02-05 15:03:58 +0800

    add compile script

---
+++ compile_in_1804_gcc9.sh
@@ -0,0 +1,9 @@
+cmake 
+  -DBUILD_BENCHMARK=OFF \
+  -DBUILD_UNIT_TESTS=OFF \
+  -DCMAKE_C_COMPILER=gcc-9 \
+  -DCMAKE_CXX_COMPILER=g++-9 \
+  -DBUILD_EXAMPLES=OFF \
+  -DENABLE_CPP_20=OFF \
+  -DCMAKE_PREFIX_PATH=/home/gsk/pro/CyberRT/wsl2_ubuntu1804 \
+  -DCMAKE_INSTALL_PREFIX=/home/gsk/pro/yalantinglibs/install/  ..
